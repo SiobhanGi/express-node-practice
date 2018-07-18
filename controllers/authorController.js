f6d43e7bc7258e4e1a261967c29f04cf52ca7d75
SiobhanGi <siobhan.goggins@gmail.com>
2018-07-19 00:45:13 +0100

    Refactor authorController.js to use arrow syntax and lint file

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,33 +1,33 @@
-let Author = require('../models/author');
+const Author = require('../models/author');
 
-exports.author_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author list');
+exports.author_list = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author list');
 };
 
-exports.author_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);
+exports.author_detail = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);
 };
 
-exports.author_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create GET');
+exports.author_create_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author create GET');
 };
 
-exports.author_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author create POST');
+exports.author_create_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author create POST');
 };
 
-exports.author_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author delete GET');
+exports.author_delete_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author delete GET');
 };
 
-exports.author_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author delete POST');
+exports.author_delete_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author delete POST');
 };
 
-exports.author_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author update GET');
+exports.author_update_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author update GET');
 };
 
-exports.author_update_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author update POST');
+exports.author_update_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Author update POST');
 };
