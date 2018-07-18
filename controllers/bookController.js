496d197007fbd13711c4a071e0d84422734cca10
SiobhanGi <siobhan.goggins@gmail.com>
2018-07-19 00:46:51 +0100

    Refactor bookController.js to use arrow syntax and lint file

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,37 +1,37 @@
-let Book = require('../models/book');
+const Book = require('../models/book');
 
-exports.index = function(req, res) {
-    res.send('NOT IMPLEMENTED: Site Home Page');
+exports.index = (req, res) => {
+  res.send('NOT IMPLEMENTED: Site Home Page');
 };
 
-exports.book_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book list');
+exports.book_list = (req, res) => {
+  res.send('NOT IMPLEMENTED: Book list');
 };
 
-exports.book_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book detail: ' + req.params.id);
+exports.book_detail = (req, res) => {
+  res.send('NOT IMPLEMENTED: Book detail: ' + req.params.id);
 };
 
-exports.book_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book create GET');
+exports.book_create_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Book create GET');
 };
 
-exports.book_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book create POST');
+exports.book_create_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Book create POST');
 };
 
-exports.book_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book delete GET');
+exports.book_delete_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Book delete GET');
 };
 
-exports.book_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book delete POST');
+exports.book_delete_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Book delete POST');
 };
 
-exports.book_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book update GET');
+exports.book_update_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Book update GET');
 };
 
-exports.book_update_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book update POST');
+exports.book_update_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Book update POST');
 };
