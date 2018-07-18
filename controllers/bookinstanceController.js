86e2317049705f0d6f319c91ab4cf9ad51d0ee67
SiobhanGi <siobhan.goggins@gmail.com>
2018-07-19 00:48:36 +0100

    Refactor bookInstanceController.js to use arrow syntax and lint file

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,33 +1,33 @@
-var BookInstance = require('../models/bookinstance');
+const BookInstance = require('../models/bookInstance');
 
-exports.bookinstance_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance list');
+exports.bookinstance_list = (req, res) => {
+  res.send('NOT IMPLEMENTED: BookInstance list');
 };
 
-exports.bookinstance_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance detail: ' + req.params.id);
+exports.bookinstance_detail = (req, res) => {
+  res.send('NOT IMPLEMENTED: BookInstance detail: ' + req.params.id);
 };
 
-exports.bookinstance_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance create GET');
+exports.bookinstance_create_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: BookInstance create GET');
 };
 
-exports.bookinstance_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance create POST');
+exports.bookinstance_create_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: BookInstance create POST');
 };
 
-exports.bookinstance_delete_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance delete GET');
+exports.bookinstance_delete_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: BookInstance delete GET');
 };
 
-exports.bookinstance_delete_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance delete POST');
+exports.bookinstance_delete_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: BookInstance delete POST');
 };
 
-exports.bookinstance_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance update GET');
+exports.bookinstance_update_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: BookInstance update GET');
 };
 
-exports.bookinstance_update_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance update POST');
+exports.bookinstance_update_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: BookInstance update POST');
 };
