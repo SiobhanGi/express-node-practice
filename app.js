1846fc9a28ec04dd205195caf4417d367d992dda
SiobhanGi <siobhan.goggins@gmail.com>
2018-07-18 18:21:24 +0100

    Setup db connection through mongoose

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://siobhangi:abc123@ds243041.mlab.com:43041/mydb');
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -22,18 +27,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
