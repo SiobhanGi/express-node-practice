64fbab65495c8489cd4ef4f4b43ac421c3b06681
SiobhanGi <siobhan.goggins@gmail.com>
2018-07-19 22:06:51 +0100

    Correct linting issues in author.js

--- models/author.js
+++ models/author.js
@@ -1,26 +1,26 @@
-let mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-let Schema = mongoose.Schema;
+const { Schema } = mongoose.Schema;
 
-let AuthorSchema = new Schema(
+const AuthorSchema = new Schema(
   {
-    first_name: {type: String, required: true, max: 100},
-    family_name: {type: String, required: true, max: 100},
-    date_of_birth: {type: Date},
-    date_of_death: {type: Date},
-  }
+    first_name: { type: String, required: true, max: 100 },
+    family_name: { type: String, required: true, max: 100 },
+    date_of_birth: { type: Date },
+    date_of_death: { type: Date },
+  },
 );
 
 AuthorSchema
-.virtual('name')
-.get(function () {
-  return this.family_name + ', ' + this.first_name;
-});
+  .virtual('name')
+  .get(function () {
+    return `${this.family_name}, ${this.first_name}`;
+  });
 
 AuthorSchema
-.virtual('url')
-.get(function () {
-  return '/catalog/author/' + this._id;
-});
+  .virtual('url')
+  .get(function () {
+    return `/catalog/author/${this.id}`;
+  });
 
 module.exports = mongoose.model('Author', AuthorSchema);
