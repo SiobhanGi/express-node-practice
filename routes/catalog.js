edfed62476e969aa4812a4e1f0d87b8d2e23be3d
SiobhanGi <siobhan.goggins@gmail.com>
2018-07-19 23:28:29 +0100

    Fix issues preventing server from running

--- routes/catalog.js
+++ routes/catalog.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { router } = express.Router();
+const router = express.Router();
 
 // Require controller modules.
 const book_controller = require('../controllers/bookController');
