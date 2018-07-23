22f1ef8091353076a5117295e687384077447b82
SiobhanGi <siobhan.goggins@gmail.com>
2018-07-23 23:28:42 +0100

    Bookinstance_detail function in bookInstanceController to render book_instance detail to bookinstance_detail pug

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -9,8 +9,19 @@ exports.bookinstance_list = (req, res, next) => {
     });
 };
 
-exports.bookinstance_detail = (req, res) => {
-  res.send(`NOT IMPLEMENTED: BookInstance detail: ${req.params.id}`);
+exports.bookinstance_detail = (req, res, next) => {
+    BookInstance.findById(req.params.id)
+    .populate('book')
+    .exec((err, bookinstance) => {
+      if (err) { return next(err); }
+      if (bookinstance==null) {
+          var err = new Error('Book copy not found');
+          err.status = 404;
+          return next(err);
+        }
+      res.render('bookinstance_detail', { title: 'Book:', bookinstance:  bookinstance});
+    })
+
 };
 
 exports.bookinstance_create_get = (req, res) => {
