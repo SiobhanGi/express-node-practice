00e5d8b5c161502e0f5591832f2c73dc38b3eb49
SiobhanGi <siobhan.goggins@gmail.com>
2018-07-24 01:36:48 +0100

    Fix linting errors in authorController.js

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,37 +1,35 @@
+const async = require('async');
 const Author = require('../models/author');
 const Book = require('../models/book');
 
-const async = require('async');
-
-exports.author_list = (req, res) => {
+exports.author_list = (req, res, next) => {
   Author.find()
     .sort([['family_name', 'ascending']])
-    .exec((err, list_authors) => {
+    .exec((err, listAuthors) => {
       if (err) { return next(err); }
-      res.render('author_list', { title: 'Author List', author_list: list_authors });
+      return res.render('author_list', { title: 'Author List', author_list: listAuthors });
     });
 };
 
 exports.author_detail = (req, res, next) => {
-    async.parallel({
-        author: (callback) => {
-            Author.findById(req.params.id)
-              .exec(callback)
-        },
-        authors_books: (callback) => {
-          Book.find({ 'author': req.params.id },'title summary')
-          .exec(callback)
-        },
-    }, (err, results) => {
-        if (err) { return next(err); }
-        if (results.author==null) {
-            var err = new Error('Author not found');
-            err.status = 404;
-            return next(err);
-        }
-        res.render('author_detail', { title: 'Author Detail', author: results.author, author_books: results.authors_books } );
-    });
-
+  async.parallel({
+    author: (callback) => {
+      Author.findById(req.params.id)
+        .exec(callback);
+    },
+    authors_books: (callback) => {
+      Book.find({ 'author': req.params.id }, 'title summary')
+        .exec(callback);
+    },
+  }, (err, results) => {
+    if (err) { return next(err); }
+    if (results.author == null) {
+      let err = new Error('Author not found');
+      err.status = 404;
+      return next(err);
+    }
+    return res.render('author_detail', { title: 'Author Detail', author: results.author, author_books: results.authors_books });
+  });
 };
 
 exports.author_create_get = (req, res) => {
